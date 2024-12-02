d423c946f8f0d13cb3a67a8302620685162239c3
Sam <git@eworkshop.fr>
2024-12-02 12:23:47 +0100

    Gestion du téléchargement de l'archive depuis Datatourisme
    - Téléchargement de l'archive à partir des éléments d'API dans .env
    - création .gitignore
    - création requirements.txt
    - fichier d'exemple .env

---
+++ datatourisme_etl/downloader.py
@@ -0,0 +1,57 @@
+import shutil
+import dotenv
+import os
+import requests
+import zipfile
+
+
+def check_file_exists(directory: str, filename: str) -> bool:
+    file_path = os.path.join(directory, filename)
+    return os.path.isfile(file_path)
+
+
+def download_datatourisme_archive() -> bool:
+    # Récupération des variables d'environnement définit dans le fichier .env
+    dotenv.load_dotenv()
+    # Répertoire de stockage de l'archive ZIP brute
+    download_path = "raw_archive"
+    os.makedirs(download_path, exist_ok=True)
+    file_path = os.path.join(download_path, "archive.zip")
+
+    # Téléchargement du fichier ZIP depuis datatourisme
+    url = os.getenv("DATATOURISME_URL_FLUX") + os.getenv("DATATOURISME_API_KEY")
+
+    if not check_file_exists("raw_archive", "archive.zip"):
+
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()  # Renvoi une exception si le code de statut de la réponse HTTP n'est pas 200
+
+            # Sauvegarde du fichier ZIP
+            with open(file_path, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+            return True
+        except requests.exceptions.RequestException as e:
+            return False
+    else:
+        pass
+
+
+def extract_data():
+    try:
+        with zipfile.ZipFile("raw_archive/archive.zip", "r") as zip_ref:
+            zip_ref.extractall("data")
+            # Destruction de l'archive téléchargée
+            shutil.rmtree("raw_archive")
+        return True
+    except zipfile.BadZipFile:
+        return False
+
+
+if __name__ == "__main__":
+    # Pour tester le script en appel direct
+    # Affiche True si le téléchargement s'est bien passé, False sinon
+    if download_datatourisme_archive():
+        extract_data()
