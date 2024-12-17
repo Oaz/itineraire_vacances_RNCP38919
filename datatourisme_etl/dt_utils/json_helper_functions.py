89fa73931e555e36d3b5567b2f4d9a8d30f2a46a
Samuel Mocher <git@eworkshop.fr>
2024-12-17 17:40:16 +0100

    Ajout de la fonction get_poi_postal_code pour récupérer le code postal dans le fichier JSON. Amélioration de la gestion des erreurs pour les dates de création (Renvoi d'une date par défaut si la clé n'est pas trouvée) et de mise à jour des points d'intérêt.

--- datatourisme_etl/dt_utils/json_helper_functions.py
+++ datatourisme_etl/dt_utils/json_helper_functions.py
@@ -16,6 +16,7 @@ def get_poi_identifier(filename: str) -> str | None:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None
 
+
 def get_poi_name(filename: str) -> str | None:
     """
     Récupère le nom du point d'intérêt dans le fichier JSON
@@ -31,6 +32,7 @@ def get_poi_name(filename: str) -> str | None:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None
 
+
 def get_poi_creation_date(filename: str) -> str | None:
     """
     Récupère la date de création du point d'intérêt dans le fichier JSON
@@ -43,8 +45,27 @@ def get_poi_creation_date(filename: str) -> str | None:
         _poi_created_date = content["creationDate"]
         return _poi_created_date
     except KeyError as e:
+        print(f"Erreur de clé: {e}. Clé introuvable.")
+        print("Renvoi d'une valeur par défaut")
+        default_timestamp = "1970-01-01T00:00:00Z"
+        return default_timestamp
+
+
+def get_poi_update_date(filename: str) -> str | None:
+    """
+    Récupère la date de mise à jour du point d'intérêt dans le fichier JSON
+    :param filename: str
+    :return: str | None si la date de mise à jour n'est pas trouvée
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = json.load(file)
+    try:
+        _poi_updated_date = content["lastUpdateDatatourisme"]
+        return _poi_updated_date
+    except KeyError as e:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None
+
 
 def find_last_update_by_label(label: str) -> str | None:
     """
@@ -59,6 +80,7 @@ def find_last_update_by_label(label: str) -> str | None:
         if item.get("label") == label:
             return item.get("lastUpdateDatatourisme")
     return None
+
 
 def get_poi_category(filename: str) -> list | None:
     """
@@ -75,6 +97,7 @@ def get_poi_category(filename: str) -> list | None:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None
 
+
 def category_cleanup(category_list: list) -> list:
     """
     Nettoie la liste des catégories en supprimant celles qui sont uniquement utiles au schéma
@@ -88,6 +111,7 @@ def category_cleanup(category_list: list) -> list:
     return [
         category for category in category_list if not category.startswith("schema:")
     ]
+
 
 def get_poi_region(filename: str) -> tuple[str, str] | tuple[None, None]:
     """
@@ -109,6 +133,7 @@ def get_poi_region(filename: str) -> tuple[str, str] | tuple[None, None]:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None, None
 
+
 def get_poi_department(filename: str) -> tuple[str, str] | tuple[None, None]:
     """
     Récupère le département du point d'intérêt dans le fichier JSON
@@ -118,12 +143,17 @@ def get_poi_department(filename: str) -> tuple[str, str] | tuple[None, None]:
     with open(filename, "r", encoding="utf-8") as file:
         content = json.load(file)
     try:
-        _poi_department_id = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"]["isPartOfDepartment"]["@id"]
-        _poi_department_name = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"]["isPartOfDepartment"]["rdfs:label"]["fr"][0]
+        _poi_department_id = content["isLocatedAt"][0]["schema:address"][0][
+            "hasAddressCity"
+        ]["isPartOfDepartment"]["@id"]
+        _poi_department_name = content["isLocatedAt"][0]["schema:address"][0][
+            "hasAddressCity"
+        ]["isPartOfDepartment"]["rdfs:label"]["fr"][0]
         return _poi_department_id, _poi_department_name
     except KeyError as e:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None, None
+
 
 def get_poi_city(filename: str) -> tuple[str, str] | tuple[None, None]:
     """
@@ -134,13 +164,36 @@ def get_poi_city(filename: str) -> tuple[str, str] | tuple[None, None]:
     with open(filename, "r", encoding="utf-8") as file:
         content = json.load(file)
     try:
-        _poi_city_id = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"]["@id"]
-        _poi_city = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"]["rdfs:label"]["fr"][0]
-        #_poi_postcode = content["isLocatedAt"][0]["schema:address"][0]["schema:postalCode"]
+        _poi_city_id = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"][
+            "@id"
+        ]
+        _poi_city = content["isLocatedAt"][0]["schema:address"][0]["hasAddressCity"][
+            "rdfs:label"
+        ]["fr"][0]
+        # _poi_postcode = content["isLocatedAt"][0]["schema:address"][0]["schema:postalCode"]
         return _poi_city_id, _poi_city
     except KeyError as e:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None, None
+
+
+def get_poi_postal_code(filename: str) -> str | None:
+    """
+    Récupère le code postal du point d'intérêt dans le fichier JSON
+    :param filename: str
+    :return: str | None si le code postal n'est pas trouvé
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = json.load(file)
+    try:
+        _poi_postcode = content["isLocatedAt"][0]["schema:address"][0][
+            "schema:postalCode"
+        ]
+        return _poi_postcode
+    except KeyError as e:
+        print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
+        return None
+
 
 def get_poi_coordinates(filename: str) -> tuple[float, float] | tuple[None, None]:
     """
@@ -157,35 +210,3 @@ def get_poi_coordinates(filename: str) -> tuple[float, float] | tuple[None, None
     except KeyError as e:
         print(f"Erreur de clé: {e}. Vérifier la structure et recommencer.")
         return None, None
-
-if __name__ == "__main__":
-    try:
-        # Essai sur un fichier JSON
-        # Pensez à modifier le chemin du fichier si nécessaire
-        file_path = "../data/objects/0/0a/49-0a1a7c7e-b2b1-3bb8-b4a2-0be8c160333a.json"
-
-        poi_identifier = get_poi_identifier(file_path)
-        poi_name = get_poi_name(file_path)
-        poi_creation_date = get_poi_creation_date(file_path)
-        poi_category = get_poi_category(file_path)
-        poi_category_cleaned = category_cleanup(poi_category)
-        poi_region_id, poi_region_name = get_poi_region(file_path)
-        poi_department_id, poi_department_name = get_poi_department(file_path)
-        poi_city_id, poi_city_name, poi_post_code = get_poi_city(file_path)
-        poi_latitude, poi_longitude = get_poi_coordinates(file_path)
-
-        print(f"Identifiant du point d'intérêt: {poi_identifier}")
-        print(f"Nom du point d'intérêt: {poi_name}")
-        print(f"Catégories *cleaned* du point d'intérêt: {poi_category_cleaned}")
-        print(f"Région du point d'intérêt: {poi_region_id} - {poi_region_name}")
-        print(f"Département du point d'intérêt: {poi_department_id} - {poi_department_name}")
-        print(f"Ville du point d'intérêt: {poi_city_id} - {poi_city_name} - {poi_post_code}")
-        print(f"Coordonnées du point d'intérêt: {poi_latitude}, {poi_longitude}")
-
-        # Récupération du last update pour un label donné
-        label_a_verifier = "Savonnerie Martin de Candre"
-        poi_last_update = find_last_update_by_label(label_a_verifier)
-        print(f"Dernière mise à jour du POI '{label_a_verifier}': {poi_last_update}")
-
-    except FileNotFoundError as e:
-        print(f"FileNotFoundError: {e}. Vérifier le chemin du fichier et recommencer.")
