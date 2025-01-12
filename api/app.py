92e5f62a63566b596f703ef573b8fb9c99e87a54
Olivier Azeau <git@azeau.com>
2025-01-13 00:53:56 +0100

    ajout d'une route /clustered_categories dans l'API
    modifications induites :
    - utilisation de utils dans l'API
    - variables de connexion aux DBs définies dans docker compose
    
    J'ai aussi rajouté un fichier examples.http utilisable depuis pycharm, entre autres. Je ne sais pas si postman utilise le même format.

--- api/app.py
+++ api/app.py
@@ -1,32 +1,46 @@
 from fastapi import FastAPI
+from utils import connect_to_db_from_env, connect_to_neo4j
 import psycopg2
 
-def get_connection():
-    return psycopg2.connect(
-        host="postgres",
-        database="datatourisme",
-        user="airflow",
-        password="airflow",
-        port="5432")
-
-
-
-app = FastAPI(title="API Itinéraire de vacances", description="API pour le calcul d'un itinéraire entre POI selon une catégorie et une localisation donnée", version="1.0.0")
+app = FastAPI(
+  title="API Itinéraire de vacances",
+  description="API pour le calcul d'un itinéraire entre POI selon une catégorie et une localisation donnée",
+  version="1.0.0"
+)
 
 
 @app.get("/db_health")
 def db_service_health():
-    try:
-        conn = get_connection()
-        return {"message": "PostgreSQL is UP", "code": 200}
-    except(Exception, psycopg2.Error) as error:
-        return {"status": error, "code": 500}
+  try:
+    conn = connect_to_db_from_env()
+    return {"message": "PostgreSQL is UP", "code": 200}
+  except(Exception, psycopg2.Error) as error:
+    return {"status": error, "code": 500}
+
 
 @app.get("/count_poi")
 def count_point_of_interest():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT COUNT(*) FROM point_of_interest")
-    record = cursor.fetchone()
-    conn.close()
-    return {"count": record[0], "code": 200}
+  conn = connect_to_db_from_env()
+  cursor = conn.cursor()
+  cursor.execute("SELECT COUNT(*) FROM point_of_interest")
+  record = cursor.fetchone()
+  conn.close()
+  return {"count": record[0], "code": 200}
+
+
+@app.get("/clustered_categories")
+def list_categories_with_associated_clusters():
+  with connect_to_neo4j() as driver:
+    with driver.session() as session:
+      result = session.run('''
+        MATCH (c:Cluster)
+        WITH c.category AS category, COUNT(c) AS clusterCount
+        MATCH (c)-[r:ROUTE]->(c2)
+        WHERE c.category = c2.category AND c.category = category
+        RETURN category, clusterCount, COUNT(r) AS routeCount
+        ''')
+      categories = [(r['category'], r['clusterCount'], r['routeCount']) for r in result]
+      return [
+        {'category': category, 'clusterCount': clusterCount, 'routeCount': routeCount}
+        for category, clusterCount, routeCount in sorted(categories)
+      ]
